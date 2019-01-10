778f7592122c47637e2c079c8c875bf8661e2cbf
kubasikora <J.Sikora@stud.elka.pw.edu.pl>
2019-01-11 00:19:18 +0100

    #23 fully functional bitmap in asm function ready to be played with

--- bezier.s
+++ bezier.s
@@ -14,12 +14,36 @@ begin:
 	cmp rax, 0
 	jz end
 
+	mov r10, 240000 ;max 480000
+
+loop:
 	mov [rax], DWORD 0xFF0000FF
 	add rax, 4
-	mov  [rax], DWORD 0x000000FF
-	add rax, 4
+	dec r10
+	cmp r10, 0
+	jnz loop
 
-	mov rax, rdi
+	;mov rax, rdi
+	mov r9, 300
+
+loop2:
+	mov r10, 200
+	mov r11, rax
+
+inner_loop2:
+	mov [rax], DWORD 0xFFFF0000
+	add rax, 4
+	dec r10
+	cmp r10, 0
+	jnz inner_loop2
+
+	mov rax, r11
+	add rax, 3200
+
+	dec r9
+	cmp r9, 0
+	jnz loop2
+
 
 end:
 	mov rsp, rbp		;epilog
--- main.c
+++ main.c
@@ -7,82 +7,56 @@
 
 #include<stdio.h>
 #include<allegro5/allegro.h>
-#include <allegro5/allegro_image.h> // plik nagłówkowy
-#include <allegro5/allegro_native_dialog.h> // plik nagłówkowy do funkcji sprawdzającej wczytanie bitmapy.
+#include <allegro5/allegro_image.h> 
+#include <allegro5/allegro_native_dialog.h> 
 
 #include"arko.h"
 
-volatile int close_button_pressed = 0;
-void close_button_handler(void);
-
-const float FPS = 60;
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
 
+void drawBezierCurve(ALLEGRO_BITMAP* bitmap, Points* points, int width, int height);
+void initializeAllegro();
+
 int main(int argc, char* argv[]){
 	ALLEGRO_DISPLAY *display = NULL;
-	ALLEGRO_TIMER *tick = NULL;
 	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
 	ALLEGRO_BITMAP *bitmap = NULL;
-	ALLEGRO_LOCKED_REGION *region = NULL;
-
+	
 	bool redraw = true;
-	unsigned char* pixelBuffer;
-
 	Points points;
 	points.num = 0;
 
-    if(!al_init()) {
-        printf("Error with allegro\n");
-		return -1;
-    }
+	initializeAllegro();
 
-    display = al_create_display(WINDOW_WIDTH, WINDOW_HEIGHT);
+	display = al_create_display(WINDOW_WIDTH, WINDOW_HEIGHT);
     if(!display){
-         printf("Error with display\n");
-		return -1;
+        printf("Error with display\n");
+		exit(-1);
     }
 
 	event_queue = al_create_event_queue();
 	if (!event_queue){
 		printf("Error with event queue\n");
-		return -1;
+		exit(-1);
 	}
 
-	tick = al_create_timer(1.0/ FPS);
-	if (!tick){
-		printf("Error with timer \n");
-		return -1;
-	}
-
-	al_set_window_title( display, "Architektura Komputerów");
-
+	al_set_window_title(display, "Architektura Komputerów");
 	bitmap = al_create_bitmap(WINDOW_WIDTH, WINDOW_HEIGHT);
-
-	al_init_image_addon();
 
 	al_set_target_bitmap(bitmap);
 	al_clear_to_color(al_map_rgb(255, 255, 255));
-
 	al_set_target_backbuffer(display);
-	
-	al_install_mouse();
 
 	al_register_event_source(event_queue, al_get_display_event_source(display));
-	al_register_event_source(event_queue, al_get_timer_event_source(tick));
 	al_register_event_source(event_queue, al_get_mouse_event_source());
 
-	al_start_timer(tick);
 	while (true)
 	{
 		ALLEGRO_EVENT ev;
 		al_wait_for_event(event_queue, &ev);
 
 		switch(ev.type){
-			case ALLEGRO_EVENT_TIMER:
-				
-				break;
-
 			case ALLEGRO_EVENT_DISPLAY_CLOSE:
 				al_save_bitmap("./bitmap.bmp", bitmap);
 				exit(0);
@@ -110,16 +84,9 @@ int main(int argc, char* argv[]){
 		}
 
 		if (redraw && al_is_event_queue_empty(event_queue)) {
-			unsigned int a;
 			redraw = false;
 
-			region = al_lock_bitmap(bitmap, ALLEGRO_PIXEL_FORMAT_ABGR_8888, ALLEGRO_LOCK_READWRITE);
-			pixelBuffer = (unsigned char*) region -> data;
-			pixelBuffer -= (-region->pitch * (WINDOW_HEIGHT -1));
-			printf("Pitch: %d, pixelSize: %d\n", region->pitch, region->pixel_size);
-			a = bezier(pixelBuffer, points.num, points.x, points.y);
-			printf("%0x\n", a);
-			al_unlock_bitmap(bitmap);
+			drawBezierCurve(bitmap, &points, WINDOW_WIDTH, WINDOW_HEIGHT);
 
 			al_clear_to_color(al_map_rgb(255, 255, 255));
 			al_draw_bitmap(bitmap, 0, 0, 0);
@@ -128,4 +95,23 @@ int main(int argc, char* argv[]){
 	}
 
 	return 0;
+}
+
+void drawBezierCurve(ALLEGRO_BITMAP* bitmap, Points* points, int width, int height){
+	unsigned char* pixelBuffer;
+	ALLEGRO_LOCKED_REGION *region = NULL;
+	region = al_lock_bitmap(bitmap, ALLEGRO_PIXEL_FORMAT_ABGR_8888, ALLEGRO_LOCK_READWRITE);
+	pixelBuffer = (unsigned char*) region -> data;
+	pixelBuffer -= (-region->pitch * (WINDOW_HEIGHT -1));
+	bezier(pixelBuffer, points->num, points->x, points->y);
+	al_unlock_bitmap(bitmap);
+}
+
+void initializeAllegro(){
+    if(!al_init()) {
+        printf("Error with allegro\n");
+		exit(-1);
+    }
+	al_install_mouse();
+	al_init_image_addon();
 }
